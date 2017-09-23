45906cfa7f2f55fd942a4c24f06b05d16e502f5a
Panda <linimbus@126.com>
2017-09-23 21:34:34 +0800

    Cache size contrast performance

--- socket/tcp/tcp.go
+++ socket/tcp/tcp.go
@@ -10,7 +10,7 @@ import (
 )
 
 const (
-	IP   = "localhost"
+	IP   = "192.168.0.107"
 	PORT = "6060"
 )
 
@@ -20,7 +20,20 @@ var recvmsgsize int
 var sendmsgcnt int
 var sendmsgsize int
 
-func netstat() {
+type banchmark struct {
+	sendbuflen  int
+	recvmsgsize int
+}
+
+var banchmarktest [15]banchmark
+
+func netstat_client() {
+
+	num := 0
+
+	time.Sleep(time.Second)
+
+	log.Println("banch mark test start...")
 
 	lastrecvmsgcnt := recvmsgcnt
 	lastrecvmsgsize := recvmsgsize
@@ -29,6 +42,55 @@ func netstat() {
 	lastsendmsgsize := sendmsgsize
 
 	for {
+
+		time.Sleep(time.Second)
+
+		log.Printf("Recv %d cnt/s , %.3f MB/s \r\n",
+			recvmsgcnt-lastrecvmsgcnt,
+			float32(recvmsgsize-lastrecvmsgsize)/(1024*1024))
+
+		log.Printf("Send %d cnt/s , %.3f MB/s \r\n",
+			sendmsgcnt-lastsendmsgcnt,
+			float32(sendmsgsize-lastsendmsgsize)/(1024*1024))
+
+		banchmarktest[num].sendbuflen = sendbuflen
+		banchmarktest[num].recvmsgsize = recvmsgsize - lastrecvmsgsize
+
+		num++
+
+		lastrecvmsgcnt = recvmsgcnt
+		lastrecvmsgsize = recvmsgsize
+
+		lastsendmsgcnt = sendmsgcnt
+		lastsendmsgsize = sendmsgsize
+
+		if sendbuflen*2 < 65535 {
+			sendbuflen = sendbuflen * 2
+		}
+
+		if num >= len(banchmarktest) {
+			log.Println("banch mark test end.")
+			break
+		}
+	}
+
+	for _, v := range banchmarktest {
+
+		log.Printf("SendBufLen %d , %.3f MB/s \r\n",
+			v.sendbuflen, float32(v.recvmsgsize)/(1024*1024))
+	}
+}
+
+func netstat_server() {
+
+	lastrecvmsgcnt := recvmsgcnt
+	lastrecvmsgsize := recvmsgsize
+
+	lastsendmsgcnt := sendmsgcnt
+	lastsendmsgsize := sendmsgsize
+
+	for {
+
 		time.Sleep(time.Second)
 
 		log.Printf("Recv %d cnt/s , %.3f MB/s \r\n",
@@ -63,7 +125,7 @@ func msgProc(conn net.Conn) {
 		recvmsgcnt++
 		recvmsgsize += n
 
-		n, err = conn.Write(buf[0:])
+		n, err = conn.Write(buf[0:n])
 		if err != nil {
 			log.Println(err.Error())
 			return
@@ -82,7 +144,7 @@ func Server() {
 		return
 	}
 
-	go netstat()
+	go netstat_server()
 
 	for {
 		conn, err2 := listen.Accept()
@@ -94,13 +156,16 @@ func Server() {
 	}
 }
 
+var sendbuflen = 128
+
 func ClientSend(conn net.Conn, wait *sync.WaitGroup) {
 
 	defer wait.Done()
-	var buf [4096]byte
+	var buf [65535]byte
 
 	for {
-		cnt, err := conn.Write(buf[0:])
+
+		cnt, err := conn.Write(buf[0:sendbuflen])
 		if err != nil {
 			log.Println(err.Error())
 			return
@@ -114,7 +179,7 @@ func ClientSend(conn net.Conn, wait *sync.WaitGroup) {
 func ClientRecv(conn net.Conn, wait *sync.WaitGroup) {
 
 	defer wait.Done()
-	var buf [4096]byte
+	var buf [65535]byte
 
 	for {
 		cnt, err := conn.Read(buf[0:])
@@ -142,7 +207,7 @@ func Client() {
 
 	go ClientSend(conn, &wait)
 	go ClientRecv(conn, &wait)
-	go netstat()
+	go netstat_client()
 
 	for i := 0; i < 100; i++ {
 		time.Sleep(time.Second)
