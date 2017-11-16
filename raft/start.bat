10bd9fb354b574d2ad55e84e1e6191d7e65d7a2e
linimbus <linimbus@126.com>
2017-11-16 23:09:36 +0800

    del

--- raft/start.bat
+++
@@ -1,3 +0,0 @@
-start init.bat localhost:8001 localhost:8002 localhost:8003
-start init.bat localhost:8002 localhost:8003 localhost:8001
-start init.bat localhost:8003 localhost:8002 localhost:8001 
