158bda820e89bb5863b8a616f90adbab8ffcf50d
linimbus <linimbus@126.com>
2017-11-25 19:28:49 +0800

    update

--- webdev/wget/main.go
+++ webdev/wget/main.go
@@ -13,13 +13,18 @@ import (
 	"time"
 )
 
+/* 例子：
+ * wget.exe -c -o home.html -p http://127.0.0.1:8080 -t 10 -u http://www.google.com
+ */
+
 var (
-	help     bool
-	crtcheck bool
-	filename string
-	proxy    string
-	path     string
-	timeout  int
+	help     bool   // help标志，查看使用帮助
+	crtcheck bool   // 是否检查证书，默认不检查
+	filename string // 输出的文件名称
+	proxy    string // http、https 访问代理
+	path     string // 需要get的url路径，需要包含完整路径：http、https://xxx
+	timeout  int    // 超时时长，单位second，默认10秒
+	retry    int    // 重试次数，默认10次
 )
 
 func usage() {
@@ -34,15 +39,15 @@ Options:
 
 func init() {
 
-	flag.BoolVar(&help, "h", false, "this help")
+	flag.BoolVar(&help, "h", false, "help")
 	flag.BoolVar(&crtcheck, "c", false, "validate the server's certificate")
-	flag.StringVar(&filename, "o", "", "output the body filename")
+	flag.StringVar(&filename, "o", "home.html", "output the body filename")
 	flag.StringVar(&proxy, "p", "", "the proxy server url")
 	flag.StringVar(&path, "u", "", "the URL need to get")
-	flag.IntVar(&timeout, "t", 0, "timeout for get by tcp connect")
+	flag.IntVar(&timeout, "t", 10, "timeout for get by tcp connect")
+	flag.IntVar(&retry, "r", 10, "retry times")
 
 	flag.Usage = usage
-
 }
 
 func readFully(conn io.ReadCloser) ([]byte, error) {
@@ -134,7 +139,7 @@ func main() {
 	}
 
 	if filename != "" {
-
+		SaveFile(body)
 	} else {
 		fmt.Fprintln(os.Stdout, string(body[:]))
 	}
