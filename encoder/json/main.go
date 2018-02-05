06dcfa4d54df88269d7d932d1555b5225532a8f3
linimbus <linimbus@126.com>
2018-02-05 21:02:07 +0800

    update

--- encoder/json/main.go
+++ encoder/json/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	"log"
 )
 
 type KvData struct {
@@ -10,6 +10,17 @@ type KvData struct {
 	Index int    `json:"LockIndex"`
 	Key   string `json:"Key"`
 	Value string `json:"Value"`
+}
+
+type ExtData struct {
+	Key   string `json:"Key2"`
+	Value string `json:"Value2"`
+}
+
+type CacheItem struct {
+	Key    string `json:"key"`
+	MaxAge int    `json:"cacheAge"`
+	Value  int    `json:"cacheValue"`
 }
 
 func main() {
@@ -22,22 +33,71 @@ func main() {
 	kv.Key = "abc"
 	kv.Value = "hello world!"
 
-	buf, err := json.Marshal(kv)
+	var extkv ExtData
+	var extkv2 ExtData
+
+	extkv.Key = "name"
+	extkv.Value = "Jack!"
+
+	buf, err := json.Marshal(struct {
+		*KvData
+		*ExtData
+	}{&kv, &extkv})
 	if err != nil {
+		log.Println(err.Error())
 		return
 	}
 
 	err = json.Unmarshal(buf, &kv2)
 	if err != nil {
+		log.Println(err.Error())
 		return
 	}
 
-	fmt.Println(kv2)
+	err = json.Unmarshal(buf, &extkv2)
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
 
-	fmt.Println("json<body>:", string(buf))
+	log.Println(kv2)
+
+	log.Println("json<body>:", string(buf))
 
 	if kv2 == kv {
-		fmt.Println("Json Marshal & Unmarshal success!")
+		log.Println("Json Marshal & Unmarshal success 1!")
 	}
+
+	if extkv2 == extkv {
+		log.Println("Json Marshal & Unmarshal success 2!")
+	}
+
+	item := &CacheItem{Key: "abcdef", MaxAge: 2, Value: 3}
+
+	buf, err = json.Marshal(struct {
+		*CacheItem
+
+		// Omit bad keys
+		OmitMaxAge int `json:"cacheAge,omitempty"`
+		OmitValue  int `json:"cacheValue,omitempty"`
+
+		// Add nice keys
+		MaxAge int  `json:"max_age"`
+		Value  *int `json:"value"`
+	}{
+		CacheItem: item,
+
+		// Set the int by value:
+		MaxAge: item.MaxAge,
+
+		// Set the nested struct by reference, avoid making a copy:
+		Value: &item.Value,
+	})
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	log.Println("json<body>:", string(buf))
 
 }
