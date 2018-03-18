da33fc60d1d77df86fa823211e6d7c03366dfaba
linimbus <linimbus@126.com>
2018-03-18 21:31:52 +0800

    update

--- webdev/tcpproxy/bridge.go
+++ webdev/tcpproxy/bridge.go
@@ -15,7 +15,72 @@ func NewTcpBridge(localaddr string, remoteaddr string) *TcpBridge {
 	return &TcpBridge{LocalAddr: localaddr, RemoteAddr: remoteaddr}
 }
 
-func (t *TcpBridge) Start() error {
+func tcpBridgeLink(localconn, remoteconn net.Conn) {
+
+	syncSem := new(sync.WaitGroup)
+
+	log.Println("new bridge connect. ", localconn.RemoteAddr(), " <-> ", remoteconn.RemoteAddr())
+
+	syncSem.Add(2)
+
+	go tcpChannel(localconn, remoteconn, syncSem)
+	go tcpChannel(remoteconn, localconn, syncSem)
+
+	syncSem.Wait()
+
+	log.Println("close bridge connect. ", localconn.RemoteAddr(), " <-> ", remoteconn.RemoteAddr())
+}
+
+func (t *TcpBridge) Bridge() error {
+
+	remoteconn := make(chan net.Conn, 10)
+	localconn := make(chan net.Conn, 10)
+
+	locallisten, err := net.Listen("tcp", t.LocalAddr)
+	if err != nil {
+		log.Println(err.Error())
+		return err
+	}
+
+	remotelisten, err := net.Listen("tcp", t.RemoteAddr)
+	if err != nil {
+		log.Println(err.Error())
+		return err
+	}
+
+	go func() {
+		for {
+			temp, err := locallisten.Accept()
+			if err != nil {
+				log.Println(err.Error())
+				continue
+			}
+			localconn <- temp
+		}
+	}()
+
+	go func() {
+		for {
+			temp, err := remotelisten.Accept()
+			if err != nil {
+				log.Println(err.Error())
+				continue
+			}
+			remoteconn <- temp
+		}
+	}()
+
+	for {
+		conn1 := <-remoteconn
+		conn2 := <-localconn
+
+		go tcpBridgeLink(conn1, conn2)
+	}
+
+	return nil
+}
+
+func (t *TcpBridge) Link() error {
 
 	localconn, err := net.Dial("tcp", t.LocalAddr)
 	if err != nil {
--- webdev/tcpproxy/main.go
+++ webdev/tcpproxy/main.go
@@ -1,14 +1,75 @@
 package main
 
 import (
+	"flag"
 	"log"
 )
 
-func main() {
-	proxy := NewTcpProxy(":20022", "192.168.0.10:22")
+// 实际中应该用更好的变量名
+var (
+	bridge     bool
+	link       bool
+	tcpproxy   bool
+	remoteaddr string
+	localaddr  string
+	help       bool
+)
 
-	err := proxy.Start()
-	if err != nil {
-		log.Println(err.Error())
+func init() {
+	flag.BoolVar(&help, "h", false, "this help")
+
+	flag.BoolVar(&bridge, "b", false, "using bridge mode.")
+	flag.BoolVar(&link, "l", false, "using link connect mode.")
+	flag.BoolVar(&tcpproxy, "t", false, "using tcp proxy mode.")
+
+	flag.StringVar(&remoteaddr, "remote", "", "remote addr")
+	flag.StringVar(&localaddr, "local", "", "local addr")
+}
+
+func main() {
+
+	flag.Parse()
+
+	if help {
+		flag.Usage()
+		return
 	}
+
+	if tcpproxy {
+
+		if remoteaddr != "" && localaddr != "" {
+			proxy := NewTcpProxy(localaddr, remoteaddr)
+
+			err := proxy.Start()
+			if err != nil {
+				log.Println(err.Error())
+			}
+			return
+		}
+
+	} else if bridge {
+
+		if remoteaddr != "" && localaddr != "" {
+			proxy := NewTcpBridge(localaddr, remoteaddr)
+
+			err := proxy.Bridge()
+			if err != nil {
+				log.Println(err.Error())
+			}
+			return
+		}
+	} else if link {
+
+		if remoteaddr != "" && localaddr != "" {
+			proxy := NewTcpBridge(localaddr, remoteaddr)
+
+			err := proxy.Link()
+			if err != nil {
+				log.Println(err.Error())
+			}
+			return
+		}
+	}
+
+	flag.Usage()
 }
